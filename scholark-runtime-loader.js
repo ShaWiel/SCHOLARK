f39bf839cde944e7dd2c23916692599c5d3e88e0
ShaWiel <famwielson@gmail.com>
2026-09-01 15:46:53 -0300

    Bump SCHOLARK runtime to R124

--- scholark-runtime-loader.js
+++ scholark-runtime-loader.js
@@ -3,7 +3,7 @@
   window.__SCHOLARK_RUNTIME_LOADER__ = true;
   window.__SCHOLARK_TEST_MODE__ = true;
 
-  const VERSION = '20260831-r123';
+  const VERSION = '20260901-r124';
   const ACTIVE = [
     'scholark-v24-ui.js','scholark-v25-enhancements.js','scholark-v27-voice-hotfix.js','scholark-v28-home-experience.js',
     'scholark-v29-home-overlay.js','scholark-v30-native-home-autodemo.js','scholark-v32-mode-preview.js','scholark-v33-preview-compat.js',
