15303983ae9ef6231a522fc3c836bfac25431c54
ShaWiel <famwielson@gmail.com>
2026-09-20 12:06:12 -0300

    R175 bump conflict-hardening release

--- scholark-v106-workspace-power-tools.js
+++ scholark-v106-workspace-power-tools.js
@@ -215,6 +215,6 @@
     focus:{state:focusState,history:focusHistory},
     flashcards:{all:cards,due:dueCards},
     assignments:{all:assignments,plan:planAssignment},
-    version:'20260920-r174'
+    version:'20260920-r175'
   };
 })();
