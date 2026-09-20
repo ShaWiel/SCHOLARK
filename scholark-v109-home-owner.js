f4d43dae560ac2214be65f64c2390e5cc5262533
ShaWiel <famwielson@gmail.com>
2026-09-20 11:47:55 -0300

    R174 integrate orchestration foundation

--- scholark-v109-home-owner.js
+++ scholark-v109-home-owner.js
@@ -58,5 +58,5 @@
   document.addEventListener('visibilitychange',()=>{if(!document.hidden)schedule()});
   schedule();
 
-  window.__SCHOLARK_V109_HOME__={prune,verify,owner:'v29-active-learning',legacyHomeLocked:true,release:'r173'};
+  window.__SCHOLARK_V109_HOME__={prune,verify,owner:'v29-active-learning',legacyHomeLocked:true,release:'r174'};
 })();
