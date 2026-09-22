3e70f15f3da376ea63e94abcb4a6be321ee30e0d
ShaWiel <famwielson@gmail.com>
2026-09-21 21:36:31 -0300

    R176: align active workspace release markers

--- scholark-v109-home-owner.js
+++ scholark-v109-home-owner.js
@@ -58,5 +58,5 @@
   document.addEventListener('visibilitychange',()=>{if(!document.hidden)schedule()});
   schedule();
 
-  window.__SCHOLARK_V109_HOME__={prune,verify,owner:'v29-active-learning',legacyHomeLocked:true,release:'r175'};
+  window.__SCHOLARK_V109_HOME__={prune,verify,owner:'v29-active-learning',legacyHomeLocked:true,release:'r176'};
 })();
