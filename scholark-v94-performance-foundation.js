3cad483ca0d4aab86925ea2628e2e6af1a7c9470
ShaWiel <famwielson@gmail.com>
2026-09-24 23:05:04 -0300

    R192: align performance foundation release

--- scholark-v94-performance-foundation.js
+++ scholark-v94-performance-foundation.js
@@ -92,5 +92,5 @@
   idle(()=>{tuneImages();fitText(activeRoot(),true)});
   setTimeout(routeTransition,350);
 
-  window.__SCHOLARK_PERF__={state,fitText,routeTransition,enableSafeMode,release:'r191'};
+  window.__SCHOLARK_PERF__={state,fitText,routeTransition,enableSafeMode,release:'r192'};
 })();
